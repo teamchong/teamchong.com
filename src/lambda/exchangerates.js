f9349bea007372dff45eb88978c584595151722f
Steven Chong <7chong+ste80@gmail.com>
2019-07-01 23:26:49 +0800

    .

--- src/lambda/exchangerates.js
+++ src/lambda/exchangerates.js
@@ -3,10 +3,6 @@ import axios from "axios";
 const { AlphaVantageApiKey } = process.env;
 
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=GBP&to_currency=HKD&apikey=${AlphaVantageApiKey}`
-  };
   try {
     const { data } = axios.get(
       `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=GBP&to_currency=HKD&apikey=${AlphaVantageApiKey}`
