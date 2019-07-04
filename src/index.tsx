0919ab8cf748527e254bd2e0a6166b5ffa8cb704
Steven Chong <7chong+ste80@gmail.com>
2019-07-04 08:58:02 +0800

    .

--- src/index.tsx
+++ src/index.tsx
@@ -90,7 +90,7 @@ const useStyles = makeStyles(theme =>
       "& img": {
         width: "100%"
       },
-      "&:hover img": {
+      "&.hover img": {
         animation: "$rotate-img 1s 1 forwards"
       },
       zIndex: 1
@@ -167,19 +167,134 @@ const useStyles = makeStyles(theme =>
   })
 );
 
-function App() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+function AboutMe({ classes }: { classes: Record<string, string> }) {
+  return (
+    <CardContent>
+      <Typography variant="h3" component="h3" className={classes.myName}>
+        Steven Chong
+      </Typography>
+      <Typography className={classes.pos} color="textSecondary">
+        - Fintech | Salesforce | Hong Kong
+      </Typography>
+      <p className="aboutme">Proud father of my daughters.</p>
+      <p className="aboutme">Experienced software development professional.</p>
+    </CardContent>
+  );
+}
 
-  function handlePopoverOpen(event) {
-    setAnchorEl(event.currentTarget);
+function SocialBar({ classes }: { classes: Record<string, string> }) {
+  return (
+    <CardContent>
+      <div className={classes.line1} />
+      <div className={classes.socialLinks}>
+        <a
+          href="https://facebook.com/chong1222"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-facebook-square" />
+        </a>
+        <a
+          href="https://github.com/teamchong"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github-square" />
+        </a>
+        <a
+          href="https://linkedin.com/in/teamchong"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin" />
+        </a>
+        <a
+          href="https://wa.me/85290388912"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-whatsapp-square" />
+        </a>
+        <a href="skype:hk90388912" className={classes.socialLinks}>
+          <i className="fab fa-skype" />
+        </a>
+        <a
+          href={`mailto:${encodeURIComponent(
+            "Steven Chong"
+          )}<steven@teamchong.com>?subject=${encodeURIComponent(
+            "[TeamChong.com] New Message"
+          )}`}
+        >
+          <i className="fas fa-envelope-square" />
+        </a>
+      </div>
+    </CardContent>
+  );
+}
+
+function AddMe({ classes }: { classes: Record<string, string> }) {
+  function handlePopoverOpen(event: React.MouseEvent<HTMLAnchorElement>) {
+    setAnchorEl({ anchorEl: event.currentTarget });
   }
 
   function handlePopoverClose() {
-    setAnchorEl(null);
+    setAnchorEl({ anchorEl: null });
   }
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+
+  return (
+    <a
+      href="StevenChong.vcf"
+      download="StevenChong.vcf"
+      className={classes.addMe}
+      onMouseEnter={handlePopoverOpen}
+      onMouseLeave={handlePopoverClose}
+    >
+      <i className="fas fa-address-card" /> Add me
+      <Popover
+        id="mouse-over-popover"
+        className={classes.popover}
+        classes={{
+          paper: classes.paper
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "right"
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <a
+          href="StevenChong.vcf"
+          download="StevenChong.vcf"
+          className={classes.addMeQr}
+        >
+          <img
+            src={vCardContent}
+            alt="ADD ME TO YOUR CONTACTS"
+            className={classes.vCardQR}
+          />
+        </a>
+      </Popover>
+    </a>
+  );
+}
+
+function App() {
+  function handleAddOil(e: React.MouseEvent<HTMLAnchorElement>) {
+    setIsAddOil(!e.currentTarget.classList.contains("hover"));
+  }
+
+  const classes = useStyles();
+  const [isAddOil, setIsAddOil] = React.useState(null);
 
   return (
     <Box
@@ -189,105 +304,17 @@ function App() {
       justifyContent="center"
     >
       <Card className={clsx(classes.card, classes.myCard)}>
-        <CardContent>
-          <Typography variant="h3" component="h3" className={classes.myName}>
-            Steven Chong
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            - Fintech | Salesforce | Hong Kong
-          </Typography>
-          <p className="aboutme">Proud father of my daughters.</p>
-          <p className="aboutme">
-            Experienced software development professional.
-          </p>
-        </CardContent>
-        <CardContent>
-          <div className={classes.line1} />
-          <div className={classes.socialLinks}>
-            <a
-              href="https://facebook.com/chong1222"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-facebook-square" />
-            </a>
-            <a
-              href="https://github.com/teamchong"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github-square" />
-            </a>
-            <a
-              href="https://linkedin.com/in/teamchong"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-linkedin" />
-            </a>
-            <a
-              href="https://wa.me/85290388912"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-whatsapp-square" />
-            </a>
-            <a href="skype:hk90388912" className={classes.socialLinks}>
-              <i className="fab fa-skype" />
-            </a>
-            <a
-              href={`mailto:${encodeURIComponent(
-                "Steven Chong"
-              )}<steven@teamchong.com>?subject=${encodeURIComponent(
-                "[TeamChong.com] New Message"
-              )}`}
-            >
-              <i className="fas fa-envelope-square" />
-            </a>
-          </div>
-        </CardContent>
+        <AboutMe classes={classes} />
+        <SocialBar classes={classes} />
       </Card>
+
+      <AddMe classes={classes} />
       <a
-        href="StevenChong.vcf"
-        download="StevenChong.vcf"
-        className={classes.addMe}
-        onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
+        className={clsx(classes.addOil, { hover: isAddOil })}
+        href="#"
+        onMouseEnter={handleAddOil}
+        onMouseOut={handleAddOil}
       >
-        <i className="fas fa-address-card" /> Add me
-        <Popover
-          id="mouse-over-popover"
-          className={classes.popover}
-          classes={{
-            paper: classes.paper
-          }}
-          open={open}
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right"
-          }}
-          transformOrigin={{
-            vertical: "bottom",
-            horizontal: "right"
-          }}
-          onClose={handlePopoverClose}
-          disableRestoreFocus
-        >
-          <a
-            href="StevenChong.vcf"
-            download="StevenChong.vcf"
-            className={classes.addMeQr}
-          >
-            <img
-              src={vCardContent}
-              alt="ADD ME TO YOUR CONTACTS"
-              className={classes.vCardQR}
-            />
-          </a>
-        </Popover>
-      </a>
-      <a href="#top" className={classes.addOil}>
         <img src={addOil} alt="香港加油" />
       </a>
       <div className={classes.bg} />
